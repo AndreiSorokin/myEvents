59cce91ba822bb41b8d755ac581f278ccaa07a69
Nguyen Duc Binh <tripplen63@gmail.com>
2024-09-16 19:35:24 +0300

    Update: - Get All locations - Update a location - Delete a location Need to fix: - Response in delete a location - Clean code

--- src/controllers/locationController.ts
+++ src/controllers/locationController.ts
@@ -3,24 +3,29 @@ import mongoose from "mongoose";
 
 import { LocationModel } from "../models/location";
 import locationService from "../services/locationService";
-import { BadRequestError, InternalServerError, NotFoundError } from "../errors/ApiError";
+import {
+  BadRequestError,
+  InternalServerError,
+  NotFoundError,
+} from "../errors/ApiError";
 
+// TODO: Create a location
 export const createLocation = async (req: Request, res: Response) => {
   try {
     console.log("Create location request body:", req.body);
     const { latitude, longitude, address } = req.body;
 
-    if(!latitude ||!longitude ||!address) {
+    if (!latitude || !longitude || !address) {
       throw new BadRequestError();
     }
 
-    const location = new LocationModel({ 
+    const location = new LocationModel({
       latitude,
       longitude,
-      address 
+      address,
     });
 
-    const newLocation = await locationService.createLocation(location)
+    const newLocation = await locationService.createLocation(location);
     res.status(201).json(newLocation);
   } catch (error: any) {
     console.error("Error creating location:", error);
@@ -28,11 +33,30 @@ export const createLocation = async (req: Request, res: Response) => {
   }
 };
 
-export const getLocationById = async (req: Request, res: Response, next: NextFunction) => {
+// TODO: Get all locations
+export const getAllLocations = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const locations = await locationService.getAllLocations();
+    res.status(200).json(locations);
+  } catch (error: any) {
+    next(new InternalServerError());
+  }
+};
+
+// TODO: Get a location by ID
+export const getLocationById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const locationId = req.params.id;
 
-    if(!locationId) {
+    if (!locationId) {
       throw new BadRequestError();
     }
 
@@ -41,18 +65,74 @@ export const getLocationById = async (req: Request, res: Response, next: NextFun
     if (!location) {
       throw new NotFoundError();
     }
-    
+
     res.status(200).json(location);
   } catch (error: any) {
-    if(error instanceof mongoose.Error.CastError) {
+    if (error instanceof mongoose.Error.CastError) {
       res.status(404).json({
-        message: "Wrond format id"
+        message: "Wrond format id",
       });
-      return
+      return;
     } else if (error instanceof NotFoundError) {
       res.status(404).json({ message: error.message });
     }
 
     next(new InternalServerError());
+  }
+};
+
+// TODO: Update a location
+export const updateLocation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const updatedLocation = await locationService.updateLocation(
+      req.params.id,
+      req.body
+    );
+    res.status(200).json(updatedLocation);
+  } catch (error: any) {
+    if (error instanceof mongoose.Error.CastError) {
+      res.status(404).json({ message: "Invalid ID format." });
+    } else if (error instanceof NotFoundError) {
+      res.status(404).json({ message: error.message });
+    } else {
+      next(new InternalServerError());
+    }
+  }
+};
+
+// TODO: Delete a location
+export const deleteLocation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const locationId = req.params.id;
+
+    if (!locationId) {
+      throw new BadRequestError("Location ID is required.");
+    }
+
+    const deletedLocation = await locationService.deleteLocation(locationId);
+
+    if (!deletedLocation) {
+      throw new NotFoundError("Location not found.");
+    }
+
+    res
+      .status(200)
+      .json({ message: "Location deleted successfully.", deletedLocation });
+  } catch (error: any) {
+    if (error instanceof mongoose.Error.CastError) {
+      res.status(404).json({ message: "Invalid ID format." });
+    } else if (error instanceof NotFoundError) {
+      res.status(404).json({ message: error.message });
+    } else {
+      next(new InternalServerError());
+    }
   }
 };
--- src/services/locationService.ts
+++ src/services/locationService.ts
@@ -2,28 +2,77 @@ import { BadRequestError, NotFoundError } from "../errors/ApiError";
 import { ILocation } from "../interfaces/ILocation";
 import { LocationModel } from "../models/location";
 
-const getLocationById = async(id: string): Promise<ILocation> => {
-  if(!id) {
-    throw new Error("Location ID is required");
-  }
-
-  const foundLocation = await LocationModel.findById(id).populate('address');
-
-  if(!foundLocation) {
-    throw new NotFoundError();
-  }
-
-  return foundLocation;
-}
-
-const createLocation = async(locationData: ILocation): Promise<ILocation> => {
+const createLocation = async (locationData: ILocation): Promise<ILocation> => {
   const { latitude, longitude, address } = locationData;
 
-  if(!latitude ||!longitude || !address) {
+  if (!latitude || !longitude || !address) {
     throw new BadRequestError();
   }
 
   return await locationData.save();
-}
+};
 
-export default { getLocationById, createLocation };
+const getAllLocations = async (): Promise<ILocation[]> => {
+  return await LocationModel.find().populate("address");
+};
+
+const getLocationById = async (id: string): Promise<ILocation> => {
+  if (!id) {
+    throw new Error("Location ID is required");
+  }
+
+  const foundLocation = await LocationModel.findById(id).populate("address");
+
+  if (!foundLocation) {
+    throw new NotFoundError();
+  }
+
+  return foundLocation;
+};
+
+// TODO: Update a location
+const updateLocation = async (
+  id: string,
+  locationData: ILocation
+): Promise<ILocation | null> => {
+  if (!id) {
+    throw new Error("Location ID is required");
+  }
+
+  const updatedLocation = await LocationModel.findByIdAndUpdate(
+    id,
+    locationData,
+    { new: true }
+  ).populate("address");
+
+  if (!updatedLocation) {
+    throw new NotFoundError(`Location with ID ${id} not found`);
+  }
+
+  return updatedLocation;
+};
+
+// TODO: Delete a location
+const deleteLocation = async (id: string): Promise<ILocation | null> => {
+  if (!id) {
+    throw new Error("Location ID is required");
+  }
+
+  const deletedLocation = await LocationModel.findByIdAndDelete(id).populate(
+    "address"
+  );
+
+  if (!deletedLocation) {
+    throw new NotFoundError(`Location with ID ${id} not found`);
+  }
+
+  return deletedLocation;
+};
+
+export default {
+  createLocation,
+  getLocationById,
+  getAllLocations,
+  updateLocation,
+  deleteLocation,
+};
