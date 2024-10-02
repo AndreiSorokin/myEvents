20159e5a2c505c6958daadfa6d6cebd5963dae4f
Viktoriia <vikatori1409801@gmail.com>
2024-10-02 20:36:43 +0300

    Start to work on unit-testing: userService

---
+++ server/src/__tests__/helper/userHelper.ts
@@ -0,0 +1,9 @@
+import { IUser } from "../../interfaces/IUser";
+
+// Helper data for tests
+export const userData = {
+  name: "John Doe",
+  email: "john@example.com",
+  password: "Password123!",
+  role: "user",
+} as IUser;
