462990f0a648fbba90ec2ef3135f72be65515ea3
Viktoriia <vikatori1409801@gmail.com>
2024-10-14 15:00:53 +0300

    Added userSlice + set up magicUI lib

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,10 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
